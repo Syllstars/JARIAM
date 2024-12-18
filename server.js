97947266e595298c3cf954d08d76c0c26062484c
Maxime PONTLEVOY <102628630+Syllstars@users.noreply.github.com>
2024-12-18 15:19:17 +0100

    Update server.js
    - Update server with the Users Routes

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const app = express();
 const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 const authRoutes  = require('./src/routes/auth');     // Chargement des routes définies dans src/routes/auth
+const userRoutes  = require('./src/routes/users');     // Chargement des routes définies dans src/routes/users
 
 // Middleware pour gérer les requêtes JSON
 app.use(express.json());
 
 // Utilisation des routes définies dans src/routes/
-app.use('/api/auth', authRoutes);                    // Toutes les routes API commenceront par '/api'
+app.use('/api/auth', authRoutes);                    // Toutes les routes API commenceront par '/api/auth'
+app.use('/api/users', userRoutes);                    // Toutes les routes API commenceront par '/api/users'
 
 // Démarrage du serveur
 app.listen(port, () => {
