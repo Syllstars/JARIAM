85793a15dac969f63d53cb84b80588152d2028e2
Maxime PONTLEVOY <102628630+Syllstars@users.noreply.github.com>
2024-12-18 15:07:06 +0100

    Integrate Routes into the main server (server.js)
    - Imported and integrated route files (auth.js) into the main server (server.js)
    - Set up route handle for API endpoint such as /api/auth.
    - Organized route integration for scanalibility and maintainability.

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
-const routes  = require('./src/routes');     // Chargement des routes définies dans src/routes
+const authRoutes  = require('./src/routes/auth');     // Chargement des routes définies dans src/routes/auth
 
 // Middleware pour gérer les requêtes JSON
-app.use(bodyParser.json());
+app.use(express.json());
 
 // Utilisation des routes définies dans src/routes/
-app.use('/api', routes);                    // Toutes les routes API commenceront par '/api'
+app.use('/api/auth', authRoutes);                    // Toutes les routes API commenceront par '/api'
 
 // Démarrage du serveur
 app.listen(port, () => {
