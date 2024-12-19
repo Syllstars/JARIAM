5401d24c0f5fa28af5ee9492f688b648f8d6c75d
Maxime PONTLEVOY <102628630+Syllstars@users.noreply.github.com>
2024-12-19 10:03:10 +0100

    Refactor authentification route in auth.js
    - Refactored the login route to handle authentification properly, including improved error handling. 
    - The route now checks for missing username or password and returns appropriate responses. 
    - A JWT is created and returned upon successful authentication with hardcoded credentials.

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,26 +1,43 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');  // Importation de jsonwebtoken
+const bcrypt = require('bcrypt');
+const { User} = require('../models');
 
-// Route pour l'authentification
-router.post('/login', (req, res) => {
-  const { username, password } = req.body;
+// Route pour l'inscription d'un utilisateur
+router.post('/register', hasRole('admin'), async (req, res) => {
+  const { username, password, email } = req.body;
+
+  if(!username || !password || !email) {
+    return res.status(400).json({ error: 'Missing username, password, or email' });
+  }
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({ username, password: hashedPassword, email });
+    res.status(201).json({ message: 'User created', user: { username, email } });
+  } catch (err) {
+    res.status(500).json({ error: 'Error registering user' });
+  }
+});
+
+// Route pour la connexion de l'utilisateur
+router.post('/login', async (req, res) => {
+  const {username, password } = req.body;
 
   if (!username || !password) {
     return res.status(400).json({ error: 'Missing username or password' });
   }
 
-  // Vérification des identifiants (ici, on suppose que "newuser" et "newpassword" sont les bons)
-  if (username === 'newuser' && password === 'newpassword') {
-    // Création du token comme avant
-    const payload = { username };
-    const token = jwt.sign(payload, 'yourSecretKey', { expiresIn: '1h' });
-
-    return res.json({ token });
+  const user = await User.findOne({ where: { username } });
+  if (!user || !(await bcrypt.compare(password, user.password))) {
+    return res.status(401).json({ error: 'Invalid credentials' });
   }
 
-  // Si les identifiants sont incorrects
-  return res.status(401).json({ message: 'Invalid credentials' });
+  const payload = { username }
+  const token = jwt.sign(payload, 'JARIAM', { expiresIn: '1h' });
+
+  res.json({ token });
 });
 
 module.exports = router;
