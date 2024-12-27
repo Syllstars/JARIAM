f3adf0cb874d556e07db21a4a1cad04d3f779989
Maxime PONTLEVOY <102628630+Syllstars@users.noreply.github.com>
2024-12-27 23:46:00 +0100

    Update notifications.js

--- src/routes/notifications.js
+++ src/routes/notifications.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { sendEmail } = require("../services/notificationService");
+const { sendEmail } = require("../services/notificationsService");
 
 const router = express.Router();
 
