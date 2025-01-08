e56b3e35d12c3d50b34a64f5c0215bb888d7ffd7
Maxime PONTLEVOY <102628630+Syllstars@users.noreply.github.com>
2025-01-08 15:11:04 +0100

    Update DashboardPage.js

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,1 +1,20 @@
+import React from 'react';
+import Sidebar from '../components/Sidebar';
+import Header from '../components/Header';
+import DashboardContent from '../components/DashboardContent';
 
+const DashboardPage = () => {
+  return (
+    <div className="dashboard-container flex">
+      { /* Barre latéral */ }
+      <Sidebar />
+      { /*Contenu principal */ }
+      <div className="main-content flex-1">
+        <Header />
+        <DashboardContent />
+      </div>
+    </div>
+  );
+};
+
+export default DashboardPage;
