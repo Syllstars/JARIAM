b9e80dc731eab5f92e3d6ae0d3c8cbdc889418cd
Maxime PONTLEVOY <102628630+Syllstars@users.noreply.github.com>
2025-01-08 16:04:46 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';                    // Importation de Tailwind CSS
+import components from './components';   // Composant principale de l'application
+
+// Rendu de l'application React dans la balise #root
+ReactDOM.render(
+  <React.StrictMode>
+    <components />
+  </React.StrictMode>
+  document.getElementById('root')
+);
