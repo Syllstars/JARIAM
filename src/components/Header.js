83e2a27988378fced39f025d1e54f169b594ffa7
Maxime PONTLEVOY <102628630+Syllstars@users.noreply.github.com>
2025-01-08 15:23:28 +0100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,15 @@
+import React from 'react';
 
+const Header = () => {
+  return (
+    <div className="header bg-white shadowmd p-4 flex justify-between items-center">
+      <h1 className="text-x1 font-bold">Dashboard</h1>
+      <div className="action flex items-center">
+        <button className="mr-4">Notifications</button>
+        <button>Profil</button>
+      </div>
+    </div>
+  );
+};
+
+export dafault Header;
